3e1d3210b60eeb448f7bd9c0ceda781ffdd63e3d
èmir <132691626+1111111111O@users.noreply.github.com>
2023-05-17 11:18:27 +0300

    en16

--- src/en/helloworld/editor.php
+++ src/en/helloworld/editor.php
@@ -7,9 +7,9 @@
   <meta charset="UTF-8">
   <h1>Prototip Editor</h1>
   <title>Editor</title>
-  <h3>Sayfa Ara ve Düzenle</h3>
-  <p>Bilgi: Bu protip bir editor sayfası,  lütfen kendi siteni düzenlemek için url kısmındaki "helloworld" kısmını oluşturduğun site adı ile değiştir.</p>
-  <p><strong><a href=#>Dosyalar</a></strong></p>
+  <h3>Page Search and Edit</h3>
+  <p>Information: This is an editor page, please replace the helloworld part in the URL section with the name of your own website to edit it.</p>
+  <p><strong><a href=#>Files</a></strong></p>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
 </head>
@@ -18,12 +18,12 @@
         <div class="input-group">
           <input type="text" id="searchInput" class="form-control" placeholder="Dosya adını girin...">
           <div class="input-group-append">
-            <button class="btn btn-primary" type="button" onclick="mesajGoster()">Ara</button>
+            <button class="btn btn-primary" type="button" onclick="mesajGoster()">Search</button>
           </div>
         </div>
         <div id="codeEditor" class="form-group mt-3" style="display: none;">
           <textarea id="codeArea" class="form-control" rows="10"></textarea>
-          <button class="btn btn-success mt-3" onclick="mesajGoster()">Kaydet</button>
+          <button class="btn btn-success mt-3" onclick="mesajGoster()">Save</button>
         </div>
 </body>
 </html>
@@ -35,50 +35,50 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <div>
-<h3>Resim yükle</h3>
+<h3>Image upload</h3>
     <input type="file" name="image" id="fileInput" />
-    <button onclick="mesajGoster()">Yükle</button>
+    <button onclick="mesajGoster()">Upload</button>
 </form>
 </div>
 
 
-	<p><strong><a href=#>Resimler</a></strong></p>
+	<p><strong><a href=#>Images</a></strong></p>
 </head>
-<h3>Sayfa Oluştur</h3>
+<h3>Create Page</h3>
       
 <form onclick="mesajGoster()">
     <div class="input-group">
         <input type="text" class="form-control" name="" required>
-        <button onclick="mesajGoster() type="submit" class="btn btn-primary">Sayfa Oluştur</button>
+        <button onclick="mesajGoster() type="submit" class="btn btn-primary">Page Create</button>
     </div>
 	
 </form>
 
 <div>
-<h3>HTML, PHP, CSS, ASP Dosyası yükle</h3>
+<h3>HTML, PHP, CSS, ASP File upload</h3>
     <input type="file" name="image" id="fileInput1" />
-    <button onclick="mesajGoster()">Yükle</button>
+    <button onclick="mesajGoster()">Upload</button>
 </form>
     <script>
         function mesajGoster() {
-            alert("Bu bir prototip, siteni düzenlemek için urldeki helloworld kısmını sitenin adı ile değiştir.");
+            alert("Information: This is an editor page, please replace the helloworld part in the URL section with the name of your own website to edit it.");
         }
     </script>
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
-	<h3>Anasayfayı düzenle</h3>
+	<h3>Edit homepage</h3>
 	<textarea id="editor" rows="10" cols="50"></textarea>
 	<br>
-	<button onclick="mesajGoster()">Kaydet</button>
+	<button onclick="mesajGoster()">Save</button>
 </body>
 </html>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Site Açılış Mesajı Örneği</title>
+    <title>Site Opening Message Example</title>
     <style>
-        /* Açılış ekranının stilini belirleyin */
+        /* Set the style of the splash screen */
         #acilisEkrani {
             position: fixed;
             top: 0;
@@ -104,12 +104,12 @@
     <div id="acilisEkrani">
         <div class="mesaj">
             <h2>Dikkat!</h2>
-            <p>Bu protip bir editor sayfası,  lütfen kendi siteni düzenlemek için url kısmındaki "helloworld" kısmını oluşturduğun site adı ile değiştir.</p>
- <button onclick="kapat()">ok</button>
+            <p>Information: This is an editor page, please replace the helloworld part in the URL section with the name of your own website to edit it.</p>
+ <button onclick="kapat()">OK</button>
         </div>
     </div>
 
-    <!-- Sayfa içeriği buraya gelecek -->
+    
 
     <script>
         window.onload = function() {
